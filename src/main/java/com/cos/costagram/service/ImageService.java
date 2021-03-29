7ab3f7f0e9ad999e3de4d7fcef90658409ac81a2
ssar <ssar@nate.com>
2021-03-29 14:12:48 +0900

    13. 인스타그램 클론코딩 - 구독자 정보보기 진행중 (1/2)

--- src/main/java/com/cos/costagram/service/ImageService.java
+++ src/main/java/com/cos/costagram/service/ImageService.java
@@ -1,11 +1,22 @@
 package com.cos.costagram.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.costagram.config.auth.PrincipalDetails;
 import com.cos.costagram.domain.image.Image;
 import com.cos.costagram.domain.image.ImageRepository;
+import com.cos.costagram.domain.tag.Tag;
+import com.cos.costagram.domain.tag.TagRepository;
+import com.cos.costagram.utils.TagUtils;
+import com.cos.costagram.web.dto.image.ImageReqDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +25,10 @@ import lombok.RequiredArgsConstructor;
 public class ImageService {
 
 	private final ImageRepository imageRepository;
+	private final TagRepository tagRepository;
+	
+	@Value("${file.path}")
+	private String uploadFolder;
 	
 	public List<Image> 피드이미지(int principalId){
 		
@@ -23,4 +38,37 @@ public class ImageService {
 		return imageRepository.mFeed(principalId);
 	}
 	
+	@Transactional
+	public void 사진업로드(ImageReqDto imageReDto, PrincipalDetails principalDetails) {
+		
+		UUID uuid = UUID.randomUUID();
+		String imageFileName = uuid+"_"+imageReDto.getFile().getOriginalFilename();
+		//System.out.println("파일명 : "+imageFileName);
+		
+		Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+		//System.out.println("파일패스 : "+imageFilePath);
+		try {
+			Files.write(imageFilePath, imageReDto.getFile().getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 참고 :  Image 엔티티에 Tag는 주인이 아니다. Image 엔티티로 통해서 Tag를 save할 수 없다.
+		
+		// 1. Image 저장
+		Image image = imageReDto.toEntity(imageFileName, principalDetails.getUser());
+		Image imageEntity = imageRepository.save(image);
+		
+		// 2. Tag 저장
+		List<Tag> tags = TagUtils.parsingToTagObject(imageReDto.getTags(), imageEntity);
+		tagRepository.saveAll(tags);
+		
+	}
 }
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/views/user/profile.jsp
+++ src/main/webapp/WEB-INF/views/user/profile.jsp
@@ -79,7 +79,7 @@
 				
 					<div class="img-box">
 						<a href="">
-							<img src="/images/profile.jpeg" alt="">
+							<img src="/upload/${image.postImageUrl}" alt="">
 						</a>
 						<div class="comment">
 							<a href="#a" class="">
@@ -132,30 +132,13 @@
 		<!--팔로워 헤더end-->
 
 		<!--팔로워 리스트-->
-		<div class="follower-list">
-			<div class="follower__item">
-				<div class="follower__img">
-					<img src="/images/profile.jpeg" alt="">
-				</div>
-				<div class="follower__text">
-					<h2>아이디</h2>
-				</div>
-				<div class="follower__btn">
-					<button onclick="clickFollow(this)">구독취소</button>
-				</div>
-			</div>
-			<div class="follower__item">
-				<div class="follower__img">
-					<img src="/images/profile.jpeg" alt="">
-				</div>
-				<div class="follower__text">
-					<h2>아이디</h2>
-				</div>
-				<div class="follower__btn">
-					<button onclick="clickFollow(this)">구독취소</button>
-				</div>
-			</div>
+		<div class="follower-list" id="follow_list">
+		
+			
+			
+			
 		</div>
+		
 		<!--팔로워 리스트end-->
 	</div>
 	<!--팔로워 박스end-->
