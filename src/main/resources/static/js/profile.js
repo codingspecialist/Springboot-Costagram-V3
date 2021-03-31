e3823c3d6a283ecabbb7a66614a5fb8967932c35
ssar <ssar@nate.com>
2021-03-31 10:13:03 +0900

    16.인스타그램 클론코딩 - 회원플로필 화면 팔로우 언팔로우 완료 그리고 복합 유니크 조건 걸기 (디비가서 테이블 다 드롭하고 다시 해야함)

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -30,9 +30,9 @@ function makeSubscribeInfo(u){
 	item += `<div class="follower__btn">`;
 	if(!u.equalState){
 		if(u.followState){
-			item += `<button class="cta blue" onclick="followOrUnFollow(${u.userId})">구독취소</button>`;	
+			item += `<button class="cta blue" onclick="followOrUnFollowModal(${u.userId})">구독취소</button>`;	
 		}else{
-			item += `<button class="cta" onclick="followOrUnFollow(${u.userId})">구독하기</button>`;
+			item += `<button class="cta" onclick="followOrUnFollowModal(${u.userId})">구독하기</button>`;
 		}	
 	}
 	item += `</div>`;
@@ -41,7 +41,7 @@ function makeSubscribeInfo(u){
 	return item;
 }
 
-function followOrUnFollow(userId){
+function followOrUnFollowModal(userId){
 	let text = $(`#follow-${userId} button`).text();
 	
 	if(text === "구독취소"){
@@ -60,11 +60,35 @@ function followOrUnFollow(userId){
 			dataType: "json"
 		}).done(res=>{
 			$(`#follow-${userId} button`).text("구독취소");
-			$(`#follow-${userId} button`).addClass("blue");
+			$(`#follow-${userId} button`).toggleClass("blue");
 		});
 	}
 }
 
+
+function followOrUnFollowProfile(userId){
+	let text = $(`#follow_profile_btn`).text();
+	
+	if(text === "구독취소"){
+		$.ajax({
+			type: "DELETE",
+			url: "/follow/"+userId,
+			dataType: "json"
+		}).done(res=>{
+			$(`#follow_profile_btn`).text("구독하기");
+			$(`#follow_profile_btn`).toggleClass("blue");
+		});
+	}else{
+		$.ajax({
+			type: "POST",
+			url: "/follow/"+userId,
+			dataType: "json"
+		}).done(res=>{
+			$(`#follow_profile_btn`).text("구독취소");
+			$(`#follow_profile_btn`).toggleClass("blue");
+		});
+	}
+}
 
 
 
